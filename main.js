e6751d0269b32b658a568945739f59c76c93f249
Sam McGrath <mcg13004@byui.edu>
2019-02-20 14:33:11 -0700

    [CORE] Made program more robust, efficient, easier to use and extended a couple of functionalities

--- main.js
+++ main.js
@@ -1,57 +1,103 @@
 const fs = require('fs');
 const canvas = require('canvas-api-wrapper');
 const prompt = require('prompt');
+const json2csv = require('json2csv').parse;
 const asyncLib = require('async');
 const {
     promisify
 } = require('util');
 const asyncReduce = promisify(asyncLib.reduce);
-const json2csv = require('json2csv').parse;
 
-var courseid;
+// var courseid;
 
 /* Prompts the user for the course Id and calls the main function to run, passing that value */
 function retrieveInput() {
     prompt.start();
 
     prompt.get('CourseID', (err, result) => {
-        courseid = result.CourseID;
+        if (err) {
+            console.log('\n\nERROR: ', err.message);
+            return;
+        } else if (!parseInt(result.CourseID, 10)) {
+            console.log('\nERROR: Invalid input.');
+            return;
+        }
+
+        checkFolders();
         runTool(result.CourseID);
     });
 }
 
 /* Makes an API call to Canvas to get a course */
 async function retrieveCourse(courseId) {
-    return await canvas.get(`/api/v1/courses/${courseId}`);
+    try {
+        return await canvas.get(`/api/v1/courses/${courseId}`);
+    } catch (err) {
+        throw err;
+    }
 }
 
 /* Makes an API call to Canvas to get all modules in the course */
 async function retrieveModules(courseId) {
-    return await canvas.get(`/api/v1/courses/${courseId}/modules`);
+    try {
+        return await canvas.get(`/api/v1/courses/${courseId}/modules`);
+    } catch (err) {
+        throw err;
+    }
 }
 
 /* Makes an API call to Canvas to get all module items in the course */
 async function retrieveModuleItems(courseId, moduleId) {
-    return await canvas.get(`/api/v1/courses/${courseId}/modules/${moduleId}/items`, {
-        "include[]": "content_details"
-    });
+    try {
+        return await canvas.get(`/api/v1/courses/${courseId}/modules/${moduleId}/items`, {
+            "include[]": "content_details"
+        });
+    } catch (err) {
+        throw err;
+    }
+}
+
+/* Simplify a portion inside runTool() */
+function returnModuleUrl(courseId, moduleId) {
+    return `https://byui.instructure.com/courses/${courseId}/modules#module_${moduleId}`;
+}
+
+function checkFolders() {
+    const courseDataPath = './courseData';
+    const htmlFilesPath = './htmlFiles';
+
+    if (!fs.existsSync(courseDataPath))
+        fs.mkdirSync(courseDataPath);
+
+    if (!fs.existsSync(htmlFilesPath))
+        fs.mkdirSync(htmlFilesPath);
 }
 
 /* Main function that runs the whole show */
 async function runTool(courseId) {
-    let courseModules = [];
-    let excludeModule = ['Welcome', 'Instructor Resources', 'Resources', 'Instructor Resources (Do NOT Publish)']
-    let modules = await retrieveModules(courseId);
-
-    /* Filters the modules to excluse the ones named in the excludeModule array */
-    modules = modules.filter(moduleIn => !excludeModule.includes(moduleIn.name));
+    let excludeModules = ['Welcome', 'Instructor Resources', 'Resources', 'Instructor Resources (Do NOT Publish)']
+    let modules;
 
     /* Maps the module item type to a number that is later interpreted into a color */
+    /* TO DO */
     let typeToColor = {
         'Discussion': 1,
         'Assignment': 2,
         'Quiz': 3
     }
+
+    //we are not sure if the courseId is valid so we wrap the function call in a try/catch statement
+    try {
+        modules = await retrieveModules(courseId);
+    } catch (err) {
+        console.log('ERROR: ', err.message);
+        return;
+    }
+
+    /* Filters the modules to exclude the ones named in the excludeModules array */
+    modules = modules.filter(moduleIn => !excludeModules.includes(moduleIn.name));
+
+    console.log('Retrieved all modules...');
 
     /* Retrieves all module items for each module */
     let allItems = await asyncReduce(modules, [], async (acc, moduleIn) => {
@@ -63,18 +109,17 @@ async function runTool(courseId) {
             .sort((a, b) => a.position - b.position)
             /* Adds the Module name and position to each module item object */
             .map((moduleItem, i) => {
-                moduleItem.position = (i + 1) * -1;
+                moduleItem.position = -(i + 1);
                 moduleItem.moduleName = moduleIn.name;
                 moduleItem.modulePosition = moduleIn.position;
                 return moduleItem;
             })
-
-            .filter(moduleItem => moduleItem.type !== "SubHeader")
+            .filter(moduleItem => moduleItem.type !== "SubHeader");
 
         return acc.concat(moduleItems);
     });
 
-
+    console.log('Retrieved all module items...');
 
     /* Creates the module item object with necessary properties */
     allItems = allItems.map(gradedItem => {
@@ -85,22 +130,26 @@ async function runTool(courseId) {
             Position: gradedItem.position,
             Points: gradedItem.content_details.points_possible || 0,
             Type: gradedItem.type,
-            URL: gradedItem.type == 'ExternalUrl' ? `https://byui.instructure.com/courses/${courseid}/modules#module_${gradedItem.module_id}` : gradedItem.html_url
-        }
+            URL: gradedItem.type === 'ExternalUrl' ? returnModuleUrl(courseId, gradedItem.module_id) : gradedItem.html_url
+        };
     });
 
+    console.log('About to create necessary files...');
+
     /* Calls to get course from Canvas (so we can retrieve the course name) */
-    let theCourse = await retrieveCourse(courseid);
-    var courseTitle = `${theCourse.course_code}-${theCourse.name}`;
+    let theCourse = await retrieveCourse(courseId);
+    let courseTitle = `${theCourse.course_code}-${theCourse.name}`;
 
     /* Converts JSON to CSV */
-    var fields = ['Name', 'ModulePosition', 'Module Name', 'Position', 'Points', 'Type', 'URL'];
-    var csv = json2csv(allItems, fields);
+    let fields = ['Name', 'ModulePosition', 'Module Name', 'Position', 'Points', 'Type', 'URL'];
+    let csv = json2csv(allItems, fields);
+
     /* Writes all data to a CSV file with the course name as the filename */
     fs.writeFileSync(`./courseData/${courseTitle}.csv`, csv, 'utf8');
 
     /* Writes all the module item objects to a file with the course name as the filename */
     fs.writeFileSync(`./courseData/${courseTitle}.json`, JSON.stringify(allItems, null, 4));
+    console.log('Files creation completed.');
 }
 
 retrieveInput();
--- makeFile.js
+++ makeFile.js
@@ -1,26 +1,36 @@
-var fs = require('fs'),
-    Handlebars = require('Handlebars'),
-    moment = require('moment');
+const fs = require('fs');
+const path = require('path');
+const moment = require('moment');
+const asyncLib = require('async');
+const {
+    promisify
+} = require('util');
+const asyncEach = promisify(asyncLib.each);
+const Handlebars = require('Handlebars');
 
-/* Filename to read in json data from (also appears as the course name in the bubble chart) */
-// TODO change this filename to the name of the json file (without the file ending)
-var fileName = 'AUTO 125-Intro to Automotive Tech';
+(async () => {
+    /* Filename to read in json data from (also appears as the course name in the bubble chart) */
+    // TODO change this filename to the name of the json file (without the file ending)
+    let files = fs.readdirSync('./courseData')
+        .filter(file => path.extname(file) !== '.csv')
 
-/* Reads in JSON data and parses it */
-var data = fs.readFileSync(`./courseData/${fileName}.json`, 'utf8');
-data = JSON.parse(data);
+    await asyncEach(files, fileName => {
+        /* Reads in JSON data and parses it */
+        let data = JSON.parse(fs.readFileSync(`./courseData/${fileName}`, 'utf8'));
 
-/* Handlebars stuff */
-var templateFile = fs.readFileSync('template.handlebars', 'utf8');
-var template = Handlebars.compile(templateFile);
+        /* Handlebars stuff */
+        let templateFile = fs.readFileSync('template.handlebars', 'utf8');
+        let template = Handlebars.compile(templateFile);
 
-var context = {
-    data: JSON.stringify(data, null, 4),
-    courseName: fileName
-};
+        let context = {
+            data: JSON.stringify(data, null, 4),
+            courseName: fileName
+        };
 
-/* Getting current date to add to filename */
-let time = moment().format('MM-DD-YY');
+        /* Getting current date to add to filename */
+        let time = moment().format('MM-DD-YY');
 
-/* Write template to a file with course code, name, and current date as the title */
-fs.writeFileSync(`./htmlFiles/${fileName} (${time}).html`, template(context), 'utf8');
+        /* Write template to a file with course code, name, and current date as the title */
+        fs.writeFileSync(`./htmlFiles/${fileName} (${time}).html`, template(context), 'utf8');
+    });
+})();
