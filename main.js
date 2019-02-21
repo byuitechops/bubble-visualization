ab6afa87e4d9e53ee6584e19a7d0efe4646cdbca
Sam McGrath <mcg13004@byui.edu>
2019-02-20 18:25:24 -0700

    [CORE/DOC] Added better function comments and created better homepage that is extendable and maintainable

--- main.js
+++ main.js
@@ -8,9 +8,12 @@ const {
 } = require('util');
 const asyncReduce = promisify(asyncLib.reduce);
 
-// var courseid;
-
-/* Prompts the user for the course Id and calls the main function to run, passing that value */
+/***************************************************
+ * retrieveInput
+ * 
+ * This function prompts the user for the course ID
+ * for course visualization. This also handles errors.
+ **************************************************/
 function retrieveInput() {
     prompt.start();
 
@@ -28,7 +31,15 @@ function retrieveInput() {
     });
 }
 
-/* Makes an API call to Canvas to get a course */
+/*************************************************
+ * retrieveCourse
+ * @param {Int} courseId
+ * @returns {Array} the course itself
+ * 
+ * This function makes an API call to Canvas and
+ * returns whatever it retrieves from the API call
+ * to the caller.
+ *************************************************/
 async function retrieveCourse(courseId) {
     try {
         return await canvas.get(`/api/v1/courses/${courseId}`);
@@ -37,7 +48,15 @@ async function retrieveCourse(courseId) {
     }
 }
 
-/* Makes an API call to Canvas to get all modules in the course */
+/*************************************************
+ * retrieveModules
+ * @param {Int} courseId
+ * @returns {Array} all of the course modules
+ * 
+ * This function makes an API call to Canvas to 
+ * retrieve all of the course modules with the 
+ * provided course ID.
+ *************************************************/
 async function retrieveModules(courseId) {
     try {
         return await canvas.get(`/api/v1/courses/${courseId}/modules`);
@@ -46,7 +65,16 @@ async function retrieveModules(courseId) {
     }
 }
 
-/* Makes an API call to Canvas to get all module items in the course */
+/*************************************************
+ * retrieveModuleItems
+ * @param {Int} courseId
+ * @param {Int} moduleId
+ * @returns {Array} module items
+ * 
+ * This function makes an API call to Canvas to 
+ * retrieve all of the module items found in the
+ * provided module ID in the provided course.
+ *************************************************/
 async function retrieveModuleItems(courseId, moduleId) {
     try {
         return await canvas.get(`/api/v1/courses/${courseId}/modules/${moduleId}/items`, {
@@ -57,11 +85,26 @@ async function retrieveModuleItems(courseId, moduleId) {
     }
 }
 
-/* Simplify a portion inside runTool() */
+/*************************************************
+ * retrieveModuleItems
+ * @param {Int} courseId
+ * @param {Int} moduleId
+ * @returns {String} Canvas link
+ * 
+ * This function returns a string for a specific
+ * module item.
+ *************************************************/
 function returnModuleUrl(courseId, moduleId) {
     return `https://byui.instructure.com/courses/${courseId}/modules#module_${moduleId}`;
 }
 
+/*************************************************
+ * checkFolders
+ * 
+ * This function checks for the two folders. If 
+ * they do not exist, this function will also 
+ * create an empty folder.
+ *************************************************/
 function checkFolders() {
     const courseDataPath = './courseData';
     const htmlFilesPath = './htmlFiles';
@@ -73,41 +116,25 @@ function checkFolders() {
         fs.mkdirSync(htmlFilesPath);
 }
 
-/* Main function that runs the whole show */
-async function runTool(courseId) {
-    let excludeModules = ['Welcome', 'Instructor Resources', 'Resources', 'Instructor Resources (Do NOT Publish)']
-    let modules;
-
-    /* Maps the module item type to a number that is later interpreted into a color */
-    /* TO DO */
-    let typeToColor = {
-        'Discussion': 1,
-        'Assignment': 2,
-        'Quiz': 3
-    }
-
-    //we are not sure if the courseId is valid so we wrap the function call in a try/catch statement
-    try {
-        modules = await retrieveModules(courseId);
-    } catch (err) {
-        console.log('ERROR: ', err.message);
-        return;
-    }
-
-    /* Filters the modules to exclude the ones named in the excludeModules array */
-    modules = modules.filter(moduleIn => !excludeModules.includes(moduleIn.name));
-
-    console.log('Retrieved all modules...');
-
+/*************************************************
+ * buildModuleItems
+ * @param {Int} courseId
+ * @param {Array} modules
+ * @returns {Array} module items for the modules
+ * 
+ * This function returns an array of all module items
+ * for each module.
+ *************************************************/
+async function buildModuleItems(courseId, modules) {
     /* Retrieves all module items for each module */
-    let allItems = await asyncReduce(modules, [], async (acc, moduleIn) => {
+    return await asyncReduce(modules, [], async (acc, moduleIn) => {
         let moduleItems = await retrieveModuleItems(courseId, moduleIn.id);
         moduleItems = moduleItems
-            /* drop the no points/ 0 points */
+            // drop the no points/ 0 points
             // .filter(item => item.content_details !== undefined && item.content_details.points_possible > 0)
-            /* make sure they are in order */
+            // make sure they are in order
             .sort((a, b) => a.position - b.position)
-            /* Adds the Module name and position to each module item object */
+            // Adds the Module name and position to each module item object
             .map((moduleItem, i) => {
                 moduleItem.position = -(i + 1);
                 moduleItem.moduleName = moduleIn.name;
@@ -118,11 +145,19 @@ async function runTool(courseId) {
 
         return acc.concat(moduleItems);
     });
+}
 
-    console.log('Retrieved all module items...');
-
-    /* Creates the module item object with necessary properties */
-    allItems = allItems.map(gradedItem => {
+/*************************************************
+ * createModuleItemObject
+ * @param {Int} courseId
+ * @param {Array} allItems
+ * @returns {Array} A JSON object for bubble chart 
+ * 
+ * This function returns an array of JSON objects
+ * for the bubble chart.
+ *************************************************/
+function createModuleItemObject(courseId, allItems) {
+    return allItems.map(gradedItem => {
         return {
             Name: gradedItem.title,
             ModulePosition: gradedItem.modulePosition,
@@ -134,21 +169,76 @@ async function runTool(courseId) {
         };
     });
 
-    console.log('About to create necessary files...');
+}
 
-    /* Calls to get course from Canvas (so we can retrieve the course name) */
+/*************************************************
+ * saveData
+ * @param {Int} courseId
+ * @param {Array} allItems
+ * 
+ * This function simply saves the data to the correct
+ * folder.
+ * 
+ *************************************************/
+async function saveData(courseId, allItems) {
+    // Calls to get course from Canvas (so we can retrieve the course name)
     let theCourse = await retrieveCourse(courseId);
     let courseTitle = `${theCourse.course_code}-${theCourse.name}`;
 
-    /* Converts JSON to CSV */
+    // Converts JSON to CSV
     let fields = ['Name', 'ModulePosition', 'Module Name', 'Position', 'Points', 'Type', 'URL'];
     let csv = json2csv(allItems, fields);
 
-    /* Writes all data to a CSV file with the course name as the filename */
+    // Writes all data to a CSV file with the course name as the filename
     fs.writeFileSync(`./courseData/${courseTitle}.csv`, csv, 'utf8');
 
-    /* Writes all the module item objects to a file with the course name as the filename */
+    // Writes all the module item objects to a file with the course name as the filename
     fs.writeFileSync(`./courseData/${courseTitle}.json`, JSON.stringify(allItems, null, 4));
+}
+
+/*************************************************
+ * runTool
+ * @param {Int} courseId
+ * 
+ * This function serves as the driver for the 
+ * program. It calls all of the correct functions.
+ *************************************************/
+async function runTool(courseId) {
+    let excludeModules = ['Welcome', 'Instructor Resources', 'Resources', 'Instructor Resources (Do NOT Publish)']
+    let modules;
+
+    // TO DO
+    // Maps the module item type to a number that is later interpreted into a color
+    let typeToColor = {
+        'Discussion': 1,
+        'Assignment': 2,
+        'Quiz': 3
+    }
+
+    // We are not sure if the courseId is valid so we wrap the function call in a try/catch statement
+    try {
+        modules = await retrieveModules(courseId);
+    } catch (err) {
+        console.log('ERROR: ', err.message);
+        return;
+    }
+
+    // Filters the modules to exclude the ones named in the excludeModules array
+    modules = modules.filter(moduleIn => !excludeModules.includes(moduleIn.name));
+
+    console.log('Retrieved all modules...');
+
+    let allItems = await buildModuleItems(courseId, modules);
+
+    console.log('Retrieved all module items...');
+
+    // Creates the module item object with necessary properties
+    allItems = createModuleItemObject(courseId, allItems);
+
+    console.log('About to create necessary files...');
+
+    await saveData(courseId, allItems);
+
     console.log('Files creation completed.');
 }
 
--- makeFile.js
+++ makeFile.js
@@ -9,28 +9,29 @@ const asyncEach = promisify(asyncLib.each);
 const Handlebars = require('Handlebars');
 
 (async () => {
-    /* Filename to read in json data from (also appears as the course name in the bubble chart) */
-    // TODO change this filename to the name of the json file (without the file ending)
+    // Grabbing only JSON files
     let files = fs.readdirSync('./courseData')
         .filter(file => path.extname(file) !== '.csv')
 
     await asyncEach(files, fileName => {
-        /* Reads in JSON data and parses it */
+        let courseName = fileName.split('-')[0];
+
+        // Reads in JSON data and parses it
         let data = JSON.parse(fs.readFileSync(`./courseData/${fileName}`, 'utf8'));
 
-        /* Handlebars stuff */
-        let templateFile = fs.readFileSync('template.handlebars', 'utf8');
+        // Handlebars stuff
+        let templateFile = fs.readFileSync('newTemplate.handlebars', 'utf8');
         let template = Handlebars.compile(templateFile);
 
         let context = {
             data: JSON.stringify(data, null, 4),
-            courseName: fileName
+            courseName: courseName
         };
 
-        /* Getting current date to add to filename */
+        // Getting current date to add to filename
         let time = moment().format('MM-DD-YY');
 
-        /* Write template to a file with course code, name, and current date as the title */
-        fs.writeFileSync(`./htmlFiles/${fileName} (${time}).html`, template(context), 'utf8');
+        // Write template to a file with course code, name, and current date as the title
+        fs.writeFileSync(`./htmlFiles/${fileName.split('.')[0]} (${time}).html`, template(context), 'utf8');
     });
 })();
